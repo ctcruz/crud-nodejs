79765cb53eccfbc9f4d8741d98335fb02b66fb6f
Tarsis Cruz <tarsis.cruz@avenuecode.com>
2024-02-21 21:51:41 -0300

    chore: add cores

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const sequelize = require("./util/database");
-
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 
